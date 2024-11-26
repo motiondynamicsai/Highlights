c873941650370dd4dcbfe91a359f675a5a849309
stumac2606 <stuartmac2606@gmail.com>
2024-11-26 12:58:33 +0000

    Track large files with Git LFS and commit changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   const [showProgressBar, setShowProgressBar] = useState(false);
   const [progress, setProgress] = useState(0);
   const [showHighlights, setShowHighlights] = useState(false);
+  const [selectedSport, setSelectedSport] = useState('squash');
 
   const handleButtonClick = () => {
     const randomSeconds = Math.floor(Math.random() * (7 - 3 + 1)) + 3; // Random number between 3 and 7
@@ -30,97 +31,109 @@ function App() {
     setProgress(0);
   };
 
+  const handleSportChange = (event) => {
+    setSelectedSport(event.target.value);
+    handleReset(); // Reset the state when changing the sport
+  };
+
+  // Define video sources and descriptions for each sport
+  const videoData = {
+    squash: [
+      { src: '/Highlights/vid1.mp4', description: 'Highlight 1: Huge rally between world no1 and 2 (26s)' },
+      { src: '/Highlights/vid2.mp4', description: 'Highlight 2: Outrageous skill level from both players (47s)' },
+      { src: '/Highlights/vid3.mp4', description: 'Highlight 3: Stunning comeback in the second game (70s)' },
+      { src: '/Highlights/vid4.mp4', description: 'Highlight 4: Unbelievable dive to win the rally (27s)' }
+    ],
+    tennis: [
+      { src: '/Highlights/TennisHL1.mp4', description: 'Tennis Highlight 1' },
+      { src: '/Highlights/TennisHL2.mp4', description: 'Tennis Highlight 2' },
+      { src: '/Highlights/TennisHL3.mp4', description: 'Tennis Highlight 3' },
+      { src: '/Highlights/TennisHL4.mp4', description: 'Tennis Highlight 4' },
+      { src: '/Highlights/TennisHL5.mp4', description: 'Tennis Highlight 5' },
+      { src: '/Highlights/TennisHL6.mp4', description: 'Tennis Highlight 6' }
+    ],
+    football: [
+      { src: '/Highlights/FootballHL1.mp4', description: 'Football Highlight 1' },
+      { src: '/Highlights/FootballHL2.mp4', description: 'Football Highlight 2' },
+      { src: '/Highlights/FootballHL3.mp4', description: 'Football Highlight 3' },
+      { src: '/Highlights/FootballHL4.mp4', description: 'Football Highlight 4' },
+      { src: '/Highlights/FootballHL5.mp4', description: 'Football Highlight 5' },
+      { src: '/Highlights/FootballHL6.mp4', description: 'Football Highlight 6' },
+      { src: '/Highlights/FootballHL7.mp4', description: 'Football Highlight 7' }
+        ],
+    padel: [
+      { src: '/Highlights/padel1.mp4', description: 'Padel Highlight 1' },
+      { src: '/Highlights/padel2.mp4', description: 'Padel Highlight 2' }
+    ]
+  };
+
+  // Define YouTube video URLs for each sport
+  const youtubeVideos = {
+    squash: "https://www.youtube.com/embed/upHZFNagiEY", // Default squash video
+    tennis: "https://www.youtube.com/embed/i29zruv8mKc?si=qDqf7JGDgS8U7NNB",
+    football: "https://www.youtube.com/embed/3j_daNHio4o?si=pAnSb0ATTtvM33jb",
+    padel: "https://www.youtube.com/embed/OTmnKkEMnDM"
+  };
+
   return (
-    <div className="App" style={{ textAlign: 'center', padding: '20px' }}>
-      <h1>Video Analysis App</h1>
-      <div style={{ margin: '20px 0' }}>
+    <div className="App">
+      <div className="highlightAI-container">
+        <p className="highlightAI">
+          Highlight<span className="ai-color">AI</span>
+        </p>
+        <select onChange={handleSportChange} value={selectedSport}>
+          <option value="squash">Squash</option>
+          <option value="tennis">Tennis</option>
+          <option value="football">Football</option>
+          <option value="padel">Padel</option>
+        </select>
+      </div>
+
+      {/* Text above the main embedded video */}
+      <div className="video-description-container">
+        <p className="video-description">
+         Coll v Farag | GillenMarkets Canary Wharf 2023 - 34 minutes
+        </p>
+      </div>
+
+      <div className="video-container">
         <iframe
           title="Video Analysis"
-          width="600"
-          height="400"
-          src="https://www.youtube.com/embed/upHZFNagiEY"
+          src={youtubeVideos[selectedSport]}
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
-          style={{ borderRadius: '8px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}
         ></iframe>
       </div>
-      <div style={{ marginBottom: '20px' }}>
-        <button
-          onClick={handleButtonClick}
-          style={{
-            padding: '10px 20px',
-            fontSize: '16px',
-            borderRadius: '5px',
-            border: 'none',
-            backgroundColor: '#007BFF',
-            color: 'white',
-            cursor: 'pointer',
-            marginRight: '10px',
-            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-          }}
-        >
-          Get Highlights
+
+      <div className="button-container">
+        <button onClick={handleButtonClick} className="btn primary-btn">
+          Generate Highlights
         </button>
-        <button
-          onClick={handleReset}
-          style={{
-            padding: '10px 20px',
-            fontSize: '16px',
-            borderRadius: '5px',
-            border: 'none',
-            backgroundColor: '#FF4D4D',
-            color: 'white',
-            cursor: 'pointer',
-            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-          }}
-        >
+        <button onClick={handleReset} className="btn reset-btn">
           Reset
         </button>
       </div>
+
       {showProgressBar && (
-        <div style={{ margin: '20px 0', width: '300px', margin: '20px auto' }}>
-          <div style={{ height: '20px', background: '#e0e0e0', borderRadius: '10px', overflow: 'hidden' }}>
-            <div
-              style={{
-                width: `${progress}%`,
-                height: '100%',
-                background: '#007BFF',
-                transition: 'width 0.1s ease',
-              }}
-            ></div>
+        <div className="progress-bar">
+          <div className="progress-bar-bg">
+            <div className="progress-bar-fill" style={{ width: `${progress}%` }}></div>
           </div>
         </div>
       )}
       {showHighlights && (
-        <div>
-          <h2>Highlights:</h2>
-          <div
-            style={{
-              display: 'grid',
-              gridTemplateColumns: 'repeat(2, 1fr)', // 2 columns for a 2x2 grid
-              gap: '20px', // Space between videos
-              justifyContent: 'center',
-              alignItems: 'center', // Center-align videos vertically
-              margin: '0 auto', // Center-align the grid
-              maxWidth: '600px', // Restrict grid width for a compact layout
-            }}
-          >
-            {['/vid1.mp4', '/vid2.mp4', '/vid3.mp4', '/vid4.mp4'].map((videoSrc, index) => (
-              <video
-                key={index}
-                width="500" // Smaller video width
-                height="auto" // Smaller video height
-                controls
-                preload="metadata"
-                style={{
-                  borderRadius: '8px',
-                  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-                }}
-              >
-                <source src={videoSrc} type="video/mp4" />
-                Your browser does not support the video tag, or the video could not be loaded.
-              </video>
+        <div className="highlights">
+          <h2>{videoData[selectedSport].length} Highlights found:</h2>
+          <div className="video-grid">
+            {videoData[selectedSport].map((video, index) => (
+              <div key={index} className="video-item">
+                <video controls preload="metadata">
+                  <source src={video.src} type="video/mp4" />
+                  Your browser does not support the video tag, or the video could not be loaded.
+                </video>
+                <p className="video-caption">{video.description}</p> {/* Display description */}
+              </div>
             ))}
           </div>
         </div>
