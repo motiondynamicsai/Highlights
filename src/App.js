0623f69777bf3710a4f64a880ec5f2193b096859
stumac2606 <stuartmac2606@gmail.com>
2024-11-26 15:58:15 +0000

    Commit full 1

--- src/App.js
+++ src/App.js
@@ -45,25 +45,30 @@ function App() {
       { src: '/Highlights/vid4.mp4', description: 'Highlight 4: Unbelievable dive to win the rally (27s)' }
     ],
     tennis: [
-      { src: '/Highlights/TennisHL1.mp4', description: 'Tennis Highlight 1' },
-      { src: '/Highlights/TennisHL2.mp4', description: 'Tennis Highlight 2' },
-      { src: '/Highlights/TennisHL3.mp4', description: 'Tennis Highlight 3' },
-      { src: '/Highlights/TennisHL4.mp4', description: 'Tennis Highlight 4' },
-      { src: '/Highlights/TennisHL5.mp4', description: 'Tennis Highlight 5' },
-      { src: '/Highlights/TennisHL6.mp4', description: 'Tennis Highlight 6' }
+      { src: '/Highlights/TennisHL1.mp4', description: 'Tennis Highlight 1: Amazing come back from 2 breaks down to win the first game (115s)' },
+      { src: '/Highlights/TennisHL2.mp4', description: 'Tennis Highlight 2: Unbelievable pass from Federer to break (22s)' },
+      { src: '/Highlights/TennisHL3.mp4', description: 'Tennis Highlight 3: Incredible power from Djokovic (10s)' },
+      { src: '/Highlights/TennisHL4.mp4', description: 'Tennis Highlight 4: Incredible atheletisism from to win the ralley (12s)'},
+      { src: '/Highlights/TennisHL5.mp4', description: 'Tennis Highlight 5: Djokovic applying pressure to take set 2 (29s)' },
+      { src: '/Highlights/TennisHL6.mp4', description: 'Tennis Highlight 6: Djokovic winning point (23s)' }
     ],
     football: [
-      { src: '/Highlights/FootballHL1.mp4', description: 'Football Highlight 1' },
-      { src: '/Highlights/FootballHL2.mp4', description: 'Football Highlight 2' },
-      { src: '/Highlights/FootballHL3.mp4', description: 'Football Highlight 3' },
-      { src: '/Highlights/FootballHL4.mp4', description: 'Football Highlight 4' },
-      { src: '/Highlights/FootballHL5.mp4', description: 'Football Highlight 5' },
-      { src: '/Highlights/FootballHL6.mp4', description: 'Football Highlight 6' },
-      { src: '/Highlights/FootballHL7.mp4', description: 'Football Highlight 7' }
+      { src: '/Highlights/FootballHL1.mp4', description: 'Football Highlight 1: Amazing 1st goal of the match (7s)' },
+      { src: '/Highlights/FootballHL2.mp4', description: 'Football Highlight 2: Great skill and strike from Messi to equalize (22s)' },
+      { src: '/Highlights/FootballHL3.mp4', description: 'Football Highlight 3: Magnificent save (4s)' },
+      { src: '/Highlights/FootballHL4.mp4', description: 'Football Highlight 4: Incredible left foot strike outside the box (9s)' },
+      { src: '/Highlights/FootballHL5.mp4', description: 'Football Highlight 5: Extremely poor challenge leads to a red card (9s)' },
+      { src: '/Highlights/FootballHL6.mp4', description: 'Football Highlight 6: Amazing ball control scoring off a cross (6s)' },
+      { src: '/Highlights/FootballHL7.mp4', description: 'Football Highlight 7: Incredible set up for a last minute winner (20s)' }
         ],
-    padel: [
-      { src: '/Highlights/padel1.mp4', description: 'Padel Highlight 1' },
-      { src: '/Highlights/padel2.mp4', description: 'Padel Highlight 2' }
+    Snow: [
+      { src: '/Highlights/snowHL1.mp4', description: 'Snow Highlight 1: Back flip into a fall' },
+      { src: '/Highlights/snowHL2.mp4', description: 'Snow Highlight 2: Outrageos trick off a kicker' },
+      { src: '/Highlights/snowHL3.mp4', description: 'Snow Highlight 3: Massive 360' },
+      { src: '/Highlights/snowHL4.mp4', description: 'Snow Highlight 4: Huge wipe out ' },
+      { src: '/Highlights/snowHL5.mp4', description: 'Snow Highlight 5: Flat spin fall to finish ' },
+      { src: '/Highlights/snowHL6.mp4', description: 'Snow Highlight 6: Unbelievable 360 into a backlip' },
+      { src: '/Highlights/snowHL7.mp4', description: 'Snow Highlight 7: Biggest backflip of the day!' }
     ]
   };
 
@@ -72,7 +77,15 @@ function App() {
     squash: "https://www.youtube.com/embed/upHZFNagiEY", // Default squash video
     tennis: "https://www.youtube.com/embed/i29zruv8mKc?si=qDqf7JGDgS8U7NNB",
     football: "https://www.youtube.com/embed/3j_daNHio4o?si=pAnSb0ATTtvM33jb",
-    padel: "https://www.youtube.com/embed/OTmnKkEMnDM"
+    Snow: "https://www.youtube.com/embed/uMW5bW8mmnY?si=TrebKhGPytIFeOnR"
+  };
+
+  // Define video descriptions for each sport
+  const videoDescriptions = {
+    squash: "Coll v Farag | GillenMarkets Canary Wharf 2023 - 34 minutes",
+    tennis: "Novak Djokovic v Roger Federer Australian Open 2020 Semifinal (1:55:28)",
+    football: "Real Madrid vs Barça (1:43:17)",
+    Snow: "Freeride World Tour 2024 (4:01:21)"
   };
 
   return (
@@ -85,14 +98,14 @@ function App() {
           <option value="squash">Squash</option>
           <option value="tennis">Tennis</option>
           <option value="football">Football</option>
-          <option value="padel">Padel</option>
+          <option value="Snow">SnowSports</option>
         </select>
       </div>
 
       {/* Text above the main embedded video */}
       <div className="video-description-container">
         <p className="video-description">
-         Coll v Farag | GillenMarkets Canary Wharf 2023 - 34 minutes
+          {videoDescriptions[selectedSport]}
         </p>
       </div>
 
